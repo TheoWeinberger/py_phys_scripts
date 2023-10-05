df8b7f86730e7e3255a5aa882f67eedbc9fa755e
Theo <theoweinberger@btconnect.com>
2023-10-05 14:48:39 +0100

    FV fix

--- fs_plot.py
+++ fs_plot.py
@@ -305,7 +305,11 @@ def read_bxsf(
             vf = np.einsum(
                 "ij,ij->i", iso1.point_data["Normals"], iso1["gradient"]
             )
-            iso1.point_data["fermi_velocity"] = 1.0 / vf
+            if scale > 1:
+                iso1.point_data["fermi_velocity"] =  -vf
+            else:
+                iso1.point_data["fermi_velocity"] =  vf
+
         isos = [iso1]
 
     # or: mesh.contour(isosurfaces=np.linspace(10, 40, 3)) etc.
@@ -450,7 +454,7 @@ def args_parser():
         metavar="\b",
         type=bool,
         help="Compute the Fermi velocity on the surface",
-        default="False",
+        default=False,
     )
 
     return parser
@@ -634,7 +638,8 @@ for file in files:
         plotter_ind.camera.azimuth = args.azimuth
         plotter_ind.camera.elevation = args.elevation
         band_index = file.split(".")[-1]
-        plotter_ind.remove_scalar_bar()
+        if args.fermi_velocity == True:
+            plotter_ind.remove_scalar_bar()
         plotter_ind.save_graphic("FS_side_" + band_index + ".pdf")
 
         counter += 1
@@ -645,7 +650,8 @@ for xx in e:
     line = pv.MultipleLines(points=np.array([xx[:, 0], xx[:, 1], xx[:, 2]]).T)
     plotter.add_mesh(line, color="black", line_width=2)
 
-plotter.remove_scalar_bar()
+if args.fermi_velocity == True:
+    plotter.remove_scalar_bar()
 plotter.set_background("white")
 plotter.set_position([0, 0, 0.5 / args.zoom])
 plotter.save_graphic("FS_top.pdf")
